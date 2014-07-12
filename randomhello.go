1a0303cbd4b6e2ea9ca650a0eab0f462cc67b754
n10o <0x0082@gmail.com>
2014-07-12 15:41:53 +0900

    Random hello

---
+++ randomhello.go
@@ -0,0 +1,18 @@
+package samgol
+
+import (
+    "math/rand"
+    "time"
+)
+
+// Return random place
+func GetPlace() string {
+    rand.Seed(time.Now().Unix())
+    places := []string{
+        "World",
+        "Singapore"
+        "渋谷",
+        "函館",
+    }
+    return places[rand.Intn(len(places))]
+}
